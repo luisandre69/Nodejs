0fdc8fb4d0cfdbe010a4e542fd8864e5d1a69efb
luisandre69 <luisandre_69@hotmail.com>
2020-07-28 14:36:52 +0100

    #Question1

--- API/test3.js
+++ API/test3.js
@@ -1,10 +1,11 @@
 const request = require('request');
-
-request('http://test3.infra.getlenses.co.uk', {
-    json: true
-}, (err, res, body) => {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(body);
-});
+setInterval(function () {
+    request('http://test3.infra.getlenses.co.uk', {
+        json: true
+    }, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(body);
+    });
+}, 30000);
